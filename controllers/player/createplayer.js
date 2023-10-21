dd6e9ed592d75610f14b1452afd8ebd13599af96
akshay <akshaytanwar1701@gmail.com>
2023-10-21 17:48:10 +0530

    changes saved

--- controllers/player/createplayer.js
+++ controllers/player/createplayer.js
@@ -7,17 +7,17 @@ const createplayer = async (req, res) => {
   try {
     const saltRounds = 10;
     const { name, email, password, address, Mobile, Role, DOB } = req.body;
-
+    // Checking user is already exists or not
     const isUser = await Player.findOne({ email: req.body.email });
 
     if (isUser) {
       res.json("user is already found");
       return;
     }
-
+    //Making salt and hash of the password
     const salt = await bcrypt.genSaltSync(saltRounds);
     const hash = await bcrypt.hashSync(password, salt);
-
+    // Saving data to the database
     const user = await Player.create({
       name: name,
       email: email,
@@ -33,6 +33,7 @@ const createplayer = async (req, res) => {
         id: user.id,
       },
     };
+    // generating token to user
     var token = jwt.sign(data, process.env.JWT_SECRET);
     res.json({ token });
   } catch (error) {
---
+++ helpers/errorhandle.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const errorhandle = (err) => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default errorhandle
